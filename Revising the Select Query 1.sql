1090bb8893a8a4fc453180bea5a61e7df7e5cb22
PongsakornSriyanyong <161577824+PongsakornSriyanyong@users.noreply.github.com>
2025-05-18 11:54:13 +0700

    Update Revising the Select Query 1.sql

--- Revising the Select Query 1.sql
+++ Revising the Select Query 1.sql
@@ -1,6 +1,6 @@
 /*
 Query all columns for all American cities in the CITY table with populations larger than 100000. 
-The CountryCode for America is USA.
+The CountryCode for America is USA.(Mysql)
 */
 select * from city
 where population > 100000 and Countrycode = "USA"
